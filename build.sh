61a2b1693c16a94d69e29a36f865951d32df5bca
Petar Matišić <93230776+pmatisic@users.noreply.github.com>
2022-05-03 02:15:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
+OUTDIR=$1
+FILE=$2
 
+OPTIONS="--synctex=1 -interaction=nonstopmode"
+
+mkdir -p $OUTDIR
+
+lualatex $OPTIONS --output-directory=$OUTDIR $FILE.tex
+biber --output-directory=$OUTDIR $FILE
+lualatex $OPTIONS --output-directory=$OUTDIR $FILE.tex
+lualatex $OPTIONS --output-directory=$OUTDIR $FILE.tex
+
+cp $OUTDIR/$FILE.pdf .
+cp $OUTDIR/$FILE.pdf petarmatisic.pdf
